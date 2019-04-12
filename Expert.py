9a3c78d4e58d757deb7973b323d72db6dc70a58d
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2019-04-12 15:07:37 -0400

    Update Expert code to use strategies directly

--- Expert.py
+++ Expert.py
@@ -4,51 +4,68 @@ class Expert(object):
     """
     An expert that takes a list of previous plays
     and formulates a prediction for the next move.
-    The expert forms its prediction based on a
-    Strategy, which describes how it selects a move.
     """
-    def __init__(self, moves, strategy):
+    def __init__(self, moves):
         self.moves = moves
-        self.strategy = strategy
 
     def predict(self, history):
         """
         Return a move prediction based on the history
         received by the expert.
         """
-        return self.strategy.choose(history)
-
-class Strategy(object):
-    def choose(self, choices):
         raise NotImplementedError
 
-class RandomStrategy(Strategy):
+class RandomExpert(Expert):
     """
-    A strategy that picks moves entirely at random.
+    An expert that picks moves entirely at random.
     """
 
-    def choose(self, choices):
+    def predict(self, history):
         """
         Return a random move from the choices.
         """
-        return random.choice(choices)
+        return random.choice(history)
 
-class KthLastMoveStrategy(Strategy):
+class KthLastMoveExpert(Expert):
     """
-    A strategy which picks the k-th last move.
+    An expert who picks the k-th last move.
     If there are fewer than k moves so far,
     pick a random move.
     """
 
-    def __init__(self, k):
+    def __init__(self, moves, k):
+        self.moves = moves
         self.k = k
 
-    def choose(self, choices):
+    def predict(self, history):
         """
         Return the k-th move from the end of the list.
         If the list is less than size k, pick randomly.
         """
-        if len(choices) > self.k:
-            return choices[-self.k]
+        if len(history) > self.k:
+            return history[-self.k]
         else:
-            return random.choice(self.moves)
+            return random.choice(history)
+
+class WeightedLastMovesExpert(Expert):
+    """
+    An expert who samples a move from a weighted history.
+    The last move has weight alpha,
+    """
+
+    def __init__(self, moves, weights):
+        self.moves = moves
+        self.weights = weights
+
+    def predict(self, history):
+        """
+        Return a randomly sampled move from the history
+        as the prediction, weighing the last k moves
+        using the given weights, where k = len(weights).
+        If the list is less than size k, pick randomly.
+        """
+        k = len(self.weights)
+        if len(history) > k:
+            return random.choices(history[k:], weights=self.weights, k=1)[0]
+        else:
+            return random.choice(history)
