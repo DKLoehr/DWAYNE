f1a15d69e8ea3abe5753438e4eae9b5fbed76a17
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2019-04-12 13:35:29 -0400

    Add Expert and Strategy classes to choose moves

---
+++ Expert.py
@@ -0,0 +1,54 @@
+import random
+
+class Expert(object):
+    """
+    An expert that takes a list of previous plays
+    and formulates a prediction for the next move.
+    The expert forms its prediction based on a
+    Strategy, which describes how it selects a move.
+    """
+    def __init__(self, moves, strategy):
+        self.moves = moves
+        self.strategy = strategy
+
+    def predict(self, history):
+        """
+        Return a move prediction based on the history
+        received by the expert.
+        """
+        return self.strategy.choose(history)
+
+class Strategy(object):
+    def choose(self, choices):
+        raise NotImplementedError
+
+class RandomStrategy(Strategy):
+    """
+    A strategy that picks moves entirely at random.
+    """
+
+    def choose(self, choices):
+        """
+        Return a random move from the choices.
+        """
+        return random.choice(choices)
+
+class KthLastMoveStrategy(Strategy):
+    """
+    A strategy which picks the k-th last move.
+    If there are fewer than k moves so far,
+    pick a random move.
+    """
+
+    def __init__(self, k):
+        self.k = k
+
+    def choose(self, choices):
+        """
+        Return the k-th move from the end of the list.
+        If the list is less than size k, pick randomly.
+        """
+        if len(choices) > k:
+            return choices[-self.k]
+        else:
+            return random.choice(choices)
