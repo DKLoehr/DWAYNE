d2039a179e49d93879f88eea3e6747e9ecfeaed7
Devon Loehr <dloehr@princeton.edu>
2019-04-19 15:17:59 -0400

    Add buttons for adding and removing moves, as well as changing the beats relation

--- GUI.py
+++ GUI.py
@@ -2,12 +2,17 @@ from tkinter import *
 from tkinter import ttk
 from functools import partial
 
+COL_OFFSET = 0
+ROW_OFFSET = 1
+
 class ButtonGrid(object):
     def __init__(self, frame, onPressExternal):
         self.buttons = {}
         self.moves = []
         self.frame = frame
         self.onPressExternal = onPressExternal
+        self.rowLabels = {}
+        self.colLabels = {}
 
     ''' Internal Functions -- do not call from outside the class '''
 
@@ -28,12 +33,16 @@ class ButtonGrid(object):
         self.buttons[move2, move1]['text'] = otherVal
         self.onPressExternal(move1, move2, newVal)
 
-    def regridAllButtons(self):
+    def regridAll(self):
         for move1 in self.moves:
+            self.rowLabels[move1].grid(row=1+ROW_OFFSET+self.moves.index(move1), column=COL_OFFSET)
+            self.colLabels[move1].grid(row=ROW_OFFSET, column=1+COL_OFFSET+self.moves.index(move1))
             for move2 in self.moves:
                 btn = self.buttons[move1, move2]
-                btn.grid_forget()
-                btn.grid(row=self.moves.index(move1), column=self.moves.index(move2), sticky=N+S+E+W)
+                #btn.grid_forget()
+                btn.grid(row=ROW_OFFSET+1+self.moves.index(move1), \
+                         column=COL_OFFSET+1+self.moves.index(move2), \
+                         sticky=N+S+E+W)
                 btn['command'] = partial(self.onPress, move1, move2)
 
     def addButton(self, move1, move2):
@@ -49,8 +58,8 @@ class ButtonGrid(object):
     def addMove(self, move):
         if move in self.moves: return
 
-        mainframe.rowconfigure(len(self.moves), weight=1)
-        mainframe.columnconfigure(len(self.moves), weight=1)
+        self.frame.rowconfigure(len(self.moves)+ROW_OFFSET+1, weight=1)
+        self.frame.columnconfigure(len(self.moves)+COL_OFFSET+1, weight=1)
 
         for move2 in self.moves:
             self.addButton(move, move2)
@@ -58,24 +67,32 @@ class ButtonGrid(object):
 
         self.moves.append(move)
         self.addButton(move, move)
-        self.regridAllButtons()
+        self.rowLabels[move] = ttk.Label(self.frame, text=move)
+        self.colLabels[move] = ttk.Label(self.frame, text=move)
+        self.regridAll()
 
     def deleteMove(self, move):
         if move not in self.moves: return
 
         self.moves.remove(move)
+        self.rowLabels[move].grid_forget()
+        del self.rowLabels[move]
+        self.colLabels[move].grid_forget()
+        del self.colLabels[move]
         self.deleteButton(move, move)
         for move2 in self.moves:
             self.deleteButton(move, move2)
             self.deleteButton(move2, move)
 
-        self.regridAllButtons()
+        self.regridAll()
 
 root = Tk()
 root.title("DWAYNE")
 
 mainframe = ttk.Frame(root)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+#mainframe.rowconfigure(0, weight=1)
+#mainframe.columnconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
@@ -83,8 +100,22 @@ def onPress(move1, move2, newVal):
     print("{},{}:{}".format(move1, move2, newVal))
 
 b = ButtonGrid(mainframe, onPress)
-b.addMove("rock")
-b.addMove("paper")
-b.addMove("scissors")
+
+activeMove = StringVar()
+
+def callAddMove():
+    b.addMove(activeMove.get())
+
+def callDelMove():
+    b.deleteMove(activeMove.get())
+
+moveEntry = ttk.Entry(mainframe, width=10, textvariable=activeMove)
+addMoveButton = ttk.Button(mainframe, text="Add Move", command=callAddMove)
+delMoveButton = ttk.Button(mainframe, text="Delete Move", command=callDelMove)
+
+moveEntry.grid(row=0, column=0, sticky=(N,W))
+addMoveButton.grid(row=0, column=1, sticky=(N))
+delMoveButton.grid(row=0, column=2, sticky=(N))
+
 
 root.mainloop()
